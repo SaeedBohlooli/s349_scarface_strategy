b97d186cbc231ef29ab885596736dacecd3a5700
Saeed Bohlooli <s.bohlooli@gmail.com>
2025-11-12 23:51:12 -0500

    Check in code ...

---
+++ utils/get_option_expires_from_nazdaq.py
@@ -0,0 +1,85 @@
+import requests
+import time
+
+import logging
+import json
+logger = logging.getLogger(__name__)
+
+
+# Try this
+# https://api.nasdaq.com/api/quote/nvda/option-chain?assetclass=stocks
+#
+# This is work around ...
+
+def get_strikes(symbol):
+    global options_meta_date_dic
+
+    try:
+        asset_class = "etf" if symbol in ["SPY", "QQQ"] else "stocks"
+        url = f"https://api.nasdaq.com/api/quote/{symbol}/option-chain?assetclass={asset_class}"
+        headers = {"User-Agent": "Mozilla/5.0"}
+        r = requests.get(url, headers=headers, timeout=10)
+        data = r.json()
+        #print(data)
+
+        rows = data.get("data", {}).get("table", {}).get("rows", [])
+        if not rows:
+            print(f"{symbol}: ⚠️ No options data found.")
+            return []
+
+        strikes = sorted({
+            float(x.get("strike") or x.get("strikePrice"))
+            for x in rows if (x.get("strike") or x.get("strikePrice"))
+        })
+        # ignore it for now. it is loke this ['None', 'Nov 14', 'Nov 21', 'Nov 28']
+        # expreis are not good ....
+        expiries = sorted({
+            str(x.get("expiryDate") or x.get("expiryDate"))
+            for x in rows if (x.get("expiryDate") or x.get("expirygroup"))
+        })
+
+        print(f"{symbol}: {len(strikes)} valid strikes")
+        strikes = list(sorted(set(strikes)))
+
+        print(strikes)
+
+        expirations = list(sorted(set(expiries)))
+        print(expirations)
+
+
+        options_meta_date_dic[symbol] = {}
+        options_meta_date_dic.get(symbol)['strikes'] = strikes
+        return strikes
+
+    except Exception as e:
+        print(f"{symbol}: ❌ Error → {e}")
+        print(data)
+        return []
+
+
+def dump_a_map_to_file(map, file_path):
+    with open(file_path, 'w') as f:
+        try:
+            logger.info(f"saving at file_path: {file_path}")
+            json.dump(map, f, indent=4)
+            logger.info(f"saving done. ")
+        except Exception as e:
+            # TODO add
+            logger.error(e)
+    return
+
+
+if __name__ == "__main__":
+
+    options_meta_date_dic = {}
+    portfolio_id = 'p250'
+    intermediate_dir = f'../../portfolios/intermediate/{portfolio_id}'
+
+    for symbol in ['QQQ', 'TSLA', 'AAPL', 'NVDA', 'AMD', 'PLTR', 'TSLL', 'AMZN', 'MNQ', 'SPY']:
+        print(f"\n===== {symbol} =====")
+        get_strikes(symbol)
+        time.sleep(1)
+
+
+    file_path = f'{intermediate_dir}/85-strikes-nazdaq.csv'
+    dump_a_map_to_file(options_meta_date_dic, file_path )
